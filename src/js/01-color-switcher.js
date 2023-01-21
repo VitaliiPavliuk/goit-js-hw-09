dcaa7d53133cfe2a9596d0e735b57a0db7440f82
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-01-21 18:46:30 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+stopBtn.setAttribute('disabled', '');
+
+const onStartBtnClick = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onStopBtnClick = () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+};
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
