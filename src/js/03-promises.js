c644881f7f763d7f4b579de6146cdbcdaab8e61d
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-01-24 01:26:15 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const promisesForm = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position: position, delay: delay });
+      } else {
+        // Reject
+        reject({ position: position, delay: delay });
+      }
+    }, delay);
+  });
 }
+
+const onSubmit = event => {
+  event.preventDefault();
+
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  const delay1 = Number(delay.value);
+  const step1 = Number(step.value);
+  const amount1 = Number(amount.value);
+
+  for (let i = 0; i < amount1; i += 1) {
+    createPromise(i + 1, delay1 + step1 * i)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+};
+
+promisesForm.addEventListener('submit', onSubmit);
